86f4f64de56a0498764ac0762b5c6a3ac20b3db8
figueiredoraissav <figueiredoraissav@gmail.com>
2026-09-23 15:30:56 -0300

    Menu no app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
   import App from "./App.jsx";
+  import Menu from "./components/Menu.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Menu/>
     <App />
   </StrictMode>,
 );
